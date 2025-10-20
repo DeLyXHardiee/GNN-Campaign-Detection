c76cb5cc73eb9a697e02cc8f95f2b61ee075bfe9
Martin Hansen <mar2000tin@gmail.com>
2025-10-20 14:06:34 +0200

    Very simple setup.

---
+++ main.py
@@ -0,0 +1,23 @@
+from TRECtoMISP import TRECtoMISPConverter
+
+def run_trec_misp_converter():
+    # Run MISP converter --> output MISP JSON file
+    TRECtoMISPConverter.csv_to_misp("data/TREC-07.csv", "results/misp/trec07_misp.json")
+
+def run_graph_creation():
+    # Run graph creation --> output PyTorch Geometric graph
+    pass
+
+def run_GNN():
+    # Run GNN model on the graph --> output embeddings
+    pass
+
+def run_clustering():
+    # Run clustering on embeddings --> store clusters
+    pass
+
+if __name__ == "__main__":
+    run_trec_misp_converter()
+    # run_graph_creation()
+    # run_GNN()
+    # run_clustering()
