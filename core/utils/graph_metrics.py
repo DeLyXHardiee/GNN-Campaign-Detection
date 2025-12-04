ed66de619e41c167e9e1744fdfc6c7254f0567ff
Martin <mwha@itu.dk>
2025-12-04 15:57:04 +0100

    Implemented collapsing for leaf nodes in graph with degree = 1

--- core/utils/graph_metrics.py
+++ core/utils/graph_metrics.py
@@ -625,8 +625,8 @@ if __name__ == "__main__":
     import os
     
     # Default paths
-    default_meta = os.path.join("../results", "trec07_misp_hetero.meta.json")
-    default_graph = os.path.join("../results", "trec07_misp_hetero.pt")
+    default_meta = os.path.join("../results", "TREC-07-misp_hetero.meta.json")
+    default_graph = os.path.join("../results", "TREC-07-misp_hetero.pt")
     
     if len(sys.argv) > 1:
         meta_path = sys.argv[1]
