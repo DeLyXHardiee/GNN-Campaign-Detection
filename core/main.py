3879ec3530b3cd0904f264fb7610e61cf8fa76f6
EXT.Anders Aversen <AAR@securedevice.dk>
2026-03-26 15:34:00 +0100

    added progress bar to graph creation

--- core/main.py
+++ core/main.py
@@ -508,11 +508,11 @@ def run_pipeline():
 
 if __name__ == "__main__":
     # For individual stages of the pipeline, uncomment as needed:
-    misp_path = run_preprocessing_lake()
+    #misp_path = run_preprocessing()
     #create_feature_sets()
     #run_featureset_clustering()
-    # misp_path = "preprocessing/output/incidents-20260211-misp.json"
-    #run_graph_creation(misp_path, to_memgraph=False)
+    misp_path = "core/preprocessing/output/incidents-lake-misp.json"
+    run_graph_creation(misp_path, to_memgraph=False)
     #run_gnn()
     #run_gnn_evaluation()
     #run_gnn_clustering()
