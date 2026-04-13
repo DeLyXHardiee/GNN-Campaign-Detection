fb89d735f5822ecb345943e1ae6faa93f0ba2f81
Main_user <sd-ops@Hypertron-Deb.securedevice.local>
2026-04-13 15:53:22 +0200

    Fixed timestamps as they were broken since retrieving from lake

--- core/main.py
+++ core/main.py
@@ -682,16 +682,16 @@ if __name__ == "__main__":
     # For individual stages of the pipeline, uncomment as needed:
     #misp_path = run_preprocessing_lake()
     #create_feature_sets()
-    run_featureset_clustering()
-    '''
+    #run_featureset_clustering()
+    
     misp_path = "preprocessing/output/incidents-lake-misp.json"
     run_graph_creation(misp_path, to_memgraph=False)
-    run_gnn()
-    run_gnn_evaluation()
-    run_gnn_clustering()
-    run_metric_comparison()
-    visualize_clusters()
+    #run_gnn()
+    #run_gnn_evaluation()
+    #run_gnn_clustering()
+    #run_metric_comparison()
+    #visualize_clusters()
     
     # To run the entire pipeline, uncomment the line below:
     # run_pipeline()
-    '''
+    
